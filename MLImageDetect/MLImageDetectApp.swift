4deb40e30077b9c8d66fe5b152c36a2bf1e2a18c
RanaAk <mahediAkbar02@gmail.com>
2024-10-23 18:20:17 +0600

    Initial Commit

---
+++ MLImageDetect/MLImageDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLImageDetectApp.swift
+//  MLImageDetect
+//
+//  Created by RANA  on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MLImageDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
